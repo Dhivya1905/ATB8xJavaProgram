6d28ef5be7433346b952d95b1deed4ef6151c0ff
Dhivya <dhivyaraman19@gmail.com>
2024-09-08 20:22:26 +0530

    Second Exercise

---
+++ src/Sept/ex_06092024/Lab004.java
@@ -0,0 +1,8 @@
+
+
+public class Lab004 {
+}
+public static void main(String[] Dhivya ) {
+  System.out.println("Hello Dhivya!");
+  System.out.println("How are you?");
+}
---
+++ src/Sept/ex_06092024/Lab005.java
@@ -0,0 +1,16 @@
+
+//Calculation Program|
+/* Author: Dhivya
+Program Name: Simple Caculation
+
+ */
+public class Lab005 {
+
+    public static void main(String[] args){
+        System.out.println (2+2);
+        System.out.println(2-2);
+        System.out.println(2*2);
+        System.out.println(2/2);
+    }
+}
+
