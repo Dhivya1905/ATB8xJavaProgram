e7db1bfa7e95f301e87507dd9769e4559445f74e
Dhivya <dhivyaraman19@gmail.com>
2024-10-19 21:58:34 +0530

    All files

---
+++ src/Oct/ex11092024_Encapsulation/Ex_2/Lab045.java
@@ -0,0 +1,20 @@
+package Oct.ex11092024_Encapsulation.Ex_2;
+
+public class Lab045 {
+
+    public void main (String [] args)
+
+    {
+ICICIBank IciciBank=new ICICIBank("Pradeep",100000);
+System.out.println(IciciBank.getName());
+System.out.println(IciciBank.getBal());
+
+boolean IsAdmin=true;
+
+        IciciBank.setName("Dhivya",false);
+        System.out.println(IciciBank.getName());
+        IciciBank.setBal(20000,false);
+        System.out.println(IciciBank.getBal());
+
+    }
+}
